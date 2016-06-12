3067648292ef286f06f04ed3b0e07fde93b96653
Eric Miller <Eric.Miller@neudesic.com>
2016-06-12 16:17:11 -0400

    testing global add of R53 records

--- environment/tools/chrome.ps1
+++ environment/tools/chrome.ps1
@@ -3,6 +3,6 @@
 $resp = Resolve-DnsName -Name test612.trycatchfinally.fail -Server ns-279.awsdns-34.com
 $cname = $resp | where {$_.Type -eq 'CNAME'}
 $cname.NameHost
-Start-Process chrome.exe -ArgumentList @( '-incognito', $cname.NameHost )
+#Start-Process chrome.exe -ArgumentList @( '-incognito', $cname.NameHost )
 #sleep -Seconds 1
 }
