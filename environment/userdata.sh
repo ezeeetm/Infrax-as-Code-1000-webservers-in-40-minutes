337031fae19bbc8e90c479c4740958ed2efaa8f6
Eric Miller <Eric.Miller@neudesic.com>
2016-06-12 15:49:36 -0400

    updated new repo name where appropriate

--- environment/userdata.sh
+++ environment/userdata.sh
@@ -5,7 +5,7 @@ apt-get install -y nginx uwsgi uwsgi-plugin-python python-pip git
 pip install virtualenv
 
 # grab our code
-git clone https://github.com/ezeeetm/1000-web-servers-on-5-continents-in-40-minutes.git
+git clone https://github.com/ezeeetm/Infrax-as-Code-1000-webservers-in-40-minutes.git
 
 # set up filesystem and virtualenv
 mkdir -p /var/www/myapp
@@ -16,10 +16,10 @@ deactivate
 chown -R www-data:www-data /var/www/myapp
 
 # copy files from local git repo out to where they belong on the filesystem
-cp -f /1000-web-servers-on-5-continents-in-40-minutes/environment/default /etc/nginx/sites-enabled/default
-cp -f /1000-web-servers-on-5-continents-in-40-minutes/environment/uwsgi.ini /etc/uwsgi/apps-enabled/uwsgi.ini
-cp -f /1000-web-servers-on-5-continents-in-40-minutes/index.py /var/www/myapp/index.py
-cp -f /1000-web-servers-on-5-continents-in-40-minutes/index.html /var/www/myapp/index.html
+cp -f /Infrax-as-Code-1000-webservers-in-40-minutes/environment/default /etc/nginx/sites-enabled/default
+cp -f /Infrax-as-Code-1000-webservers-in-40-minutes/environment/uwsgi.ini /etc/uwsgi/apps-enabled/uwsgi.ini
+cp -f /Infrax-as-Code-1000-webservers-in-40-minutes/index.py /var/www/myapp/index.py
+cp -f /Infrax-as-Code-1000-webservers-in-40-minutes/index.html /var/www/myapp/index.html
 
 # restart services to load new configs and engage uwsgi to our app
 service nginx restart
