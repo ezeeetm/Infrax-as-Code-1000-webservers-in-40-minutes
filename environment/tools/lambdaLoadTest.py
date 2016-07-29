7f311c2fa13a14c89027fb6cb1f4b5f140c34a76
Eric Miller <Eric.Miller@neudesic.com>
2016-07-28 21:39:02 -0400

    100!

--- environment/tools/lambdaLoadTest.py
+++ environment/tools/lambdaLoadTest.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 import dns.resolver
 import urllib2
+import threading
+from time import sleep
+
+def worker(url):
+    """thread worker function"""
+    resp = urllib2.urlopen(url).read()
+    return
 
 # 205.251.199.104 is ns-1896.awsdns-45.co.uk
 resolver = dns.resolver.Resolver()
 resolver.nameservers = ['205.251.199.104']
 
-answer = resolver.query('test7281.trycatchfinally.fail', 'CNAME')
-for rdata in answer:
-    url = "http://%s" % rdata
-    print url
-    #resp = urllib2.urlopen(url).read()
-    #print resp
-
-
-
+while True:
+    sleep(0.01) #100ms buffer to keep from getting throttled
+    answer = resolver.query('test7281.trycatchfinally.fail', 'CNAME')
+    for rdata in answer:
+        url = ("http://%s" % rdata)[:-1]
+        print url
+        threads = []
+        for i in range(1): # this is the # of threads
+            t = threading.Thread(target=worker, args=(url,))
+            threads.append(t)
+            t.start()
 
 # on lambda AMI, do sudo pip install dnspython
